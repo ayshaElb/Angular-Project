b1806c9d58492db943ef13df4a3b4e154558b98b
Ayshae <ayshaelbach@gmail.com>
2020-02-18 10:55:57 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
-import { NgModule } from '@angular/core';
+// app.routing.ts
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomepageComponent } from './homepage/homepage.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { PostComponent } from './post/post.component';
+import { AuthGuard } from './_guard/index';
 
-const routes: Routes = [];
+const APP_ROUTES: Routes = [
+    {
+        path: '',
+        component: HomepageComponent
+    },
+    {
+        path: 'login',
+        component: AuthenticationComponent
+    },
+    {
+        path: 'post',
+        component: PostComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: '**', redirectTo: '' }
+];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const Routing = RouterModule.forRoot(APP_ROUTES);
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
+// app.module.ts
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Http, HttpModule, RequestOptions } from '@angular/http';
+import { AuthHttp, AuthConfig } from 'angular2-jwt';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Routing } from './app-routing.module';
+import { AuthGuard } from './_guard/index';
+
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { AuthenticationService } from './authentication/authentication.service';
+import { HomepageComponent } from './homepage/homepage.component';
+import { PostComponent } from './post/post.component';
+import { PostRepository } from './post/post-repository.service';
+
+export function authHttpServiceFactory(http: Http, options: RequestOptions) {
+    return new AuthHttp( new AuthConfig({}), http, options);
+}
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+     declarations: [
+         AppComponent,
+         AuthenticationComponent,
+         HomepageComponent,
+         PostComponent
+     ],
+     imports: [
+         BrowserModule,
+         ReactiveFormsModule,
+         HttpModule,
+         Routing
+     ],
+     providers: [
+     {
+         provide: AuthHttp,
+         useFactory: authHttpServiceFactory,
+         deps: [ Http, RequestOptions ]
+     },
+         AuthGuard,
+         AuthenticationService,
+         PostRepository
+     ],
+     bootstrap: [AppComponent]
 })
 export class AppModule { }
